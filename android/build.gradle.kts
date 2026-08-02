e190eaeb3ba179ec34803d3c8f93425e36d162fe
raullavita <smashmaxxx@gmail.com>
2026-08-02 13:12:43 +0100

    fix(build): unblock android and windows desktop local builds

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
